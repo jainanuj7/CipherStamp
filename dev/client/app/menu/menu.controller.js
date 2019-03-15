7a1c7a24b208458a42a2d12c2a6efed6fe785657
Anuj Jain <lookforaj@gmail.com>
2019-03-16 01:32:41 +0530

    Added landing page

--- dev/client/app/menu/menu.controller.js
+++ dev/client/app/menu/menu.controller.js
@@ -4,13 +4,17 @@
         .module('MyBlockchain')
         .controller('MenuCtrl', MenuCtrl);
 
-    function MenuCtrl($scope, navService) {
+    function MenuCtrl($scope, navService, $window) {
         
         $scope.menuItems = [];
         navService.loadMenuItems()
             .then(function(menuItems) {
                 $scope.menuItems = [].concat(menuItems);
         });
+
+        $scope.redirectHome = function() {
+            $window.location.href = '/home.html';
+        }
 
     }
 
